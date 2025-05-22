dd4811369779204c331e65629e7d1cfc2bb90746
adfgx <arapopis@gmail.com>
2025-05-21 21:33:50 -0400

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,17 +73,7 @@
 
 	// Parallax.
 	// Disabled on IE (choppy scrolling) and mobile platforms (poor performance).
-		if (browser.name == 'ie'
-		||	browser.mobile) {
-
-			$.fn._parallax = function() {
-
-				return $(this);
-
-			};
-
-		}
-		else {
+		 {
 
 			$.fn._parallax = function() {
 
